163c3d13e6d045cfb5383d936c54dae1fdf6f544
M. Valkenburg <m.valkenburg@live.nl>
2022-08-23 16:00:25 +0200

    added products

---
+++ src/data/products/starwars.ts
@@ -0,0 +1,333 @@
+import { Product } from './_productType';
+
+export let starwars: Product[] = [
+    {
+        id: 75192,
+        name: 'Millennium Falcon™',
+        description: [
+            `Welcome to the largest, most detailed LEGO® Star Wars Millennium Falcon model we've ever created—in fact, with 7,500 pieces it's one of our biggest 
+            LEGO models, period! This amazing LEGO interpretation of Han Solo's unforgettable Corellian freighter has all the details that Star Wars fans of any 
+            age could wish for, including intricate exterior detailing, upper and lower quad laser cannons, landing legs, lowering boarding ramp and a 4-minifigure 
+            cockpit with detachable canopy. Remove individual hull plates to reveal the highly detailed main hold, rear compartment and gunnery station. This amazing 
+            model also features interchangeable sensor dishes and crew, so you decide whether to play out classic LEGO Star Wars adventures with Han, Leia, Chewbacca 
+            and C-3PO, or enter the world of Episode VII and VIII with older Han, Rey, Finn and BB-8!`
+        ],
+        pieces: 7541,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies', 'space'],
+        price: 849.99,
+        stock: 1,
+    },
+    {
+        id: 75252,
+        name: 'Imperial Star Destroyer™',
+        description: [
+            `Build and display an icon of the Galactic Empire - the Devastator. With over 4,700 LEGO® pieces, this Ultimate Collector Series 75252 Imperial Star 
+            Destroyer model captures all the authentic details of the starship as it appeared in the opening scene of Star Wars: A New Hope, including swiveling guns, 
+            a tilting radar dish, huge engine exhausts, intricate surface detailing and of course a buildable scale version of the Rebels' Tantive IV starship to chase 
+            down. This galactic civil war UCS set also includes a display stand with informational fact plaque and 2 Imperial minifigures, making it the perfect LEGO 
+            Star Wars collectible for discerning fans.`
+        ],
+        pieces: 4784,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies', 'space'],
+        price: 699.99,
+        stock: 2,
+    },
+    {
+        id: 75255,
+        name: 'Yoda™',
+        description: [
+            `Add to any fan's collection with two Yoda LEGO® Star Wars™ characters in one set! This intricately detailed 75255 display model of powerful Jedi Master Yoda 
+            is based on the character from Star Wars: Attack of the Clones, and includes a posable head and eyebrows, moving fingers and toes, and Yoda's signature green 
+            Lightsaber. This characterful LEGO Star Wars figure also includes a fact plaque with details about Yoda and a stand to mount the included Yoda minifigure with 
+            Lightsaber, making for an ideal Star Wars collectible for any fan.`
+        ],
+        pieces: 1771,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 99.99,
+        stock: 6,
+    },
+    {
+        id: 75277,
+        name: 'Boba Fett™ Helmet',
+        description: [
+            `Build and display a LEGO® clone of one of the galaxy's deadliest bounty hunters with this Boba Fett Helmet (75277) model kit! It's a super LEGO Star Wars™ 
+            gift idea for adult fans and offers a testing challenge for experienced LEGO builders, with the familiar shape and authentic details of the helmet 
+            beautifully recreated using LEGO bricks.`,
+            `Build to display<br>
+            This LEGO Boba Fett collectible has a base with a nameplate to display it at home, in the office or wherever else you like in the galaxy. For another building 
+            challenge, check out the new-for-January-2020 LEGO Star Wars Stormtrooper Helmet (75276) model kit.`
+        ],
+        pieces: 625,
+        released: true,
+        theme1: 'star wars',
+        tags: ['helmets', 'movies'],
+        price: 59.99,
+        stock: 11,
+    },
+    {
+        id: 75290,
+        name: 'Mos Eisley Cantina™',
+        description: [
+            `Enjoy quality me-time and relive iconic Star Wars: A New Hope moments with the awesome LEGO® Star Wars™ Mos Eisley Cantina (75290) construction model for display. 
+            This 3,187-piece Master Builder Series set features the Cantina with lots of authentic details to delight fans, plus attachable buildings to recreate a Mos Eisley city scene.`,
+            `Build and display<br>
+            This immersive build is a great way to relax and reconnect with your Star Wars passion. A Dewback LEGO figure and 2 Landspeeders complete a wonderful display piece. 
+            Bring your Tatooine scene to life with 21 LEGO minifigures, including Luke Skywalker, Han Solo and C-3PO, new-for-October-2020 Ponda Baba, Garindan and Labria, plus an R2-D2 LEGO droid figure.`
+        ],
+        pieces: 3187,
+        released: true,
+        theme1: 'star wars',
+        tags: ['building', 'movies'],
+        price: 399.99,
+        stock: 4,
+    },
+    {
+        id: 75296,
+        name: 'Darth Vader™ Meditation Chamber',
+        description: [
+            `“You have failed me for the last time, Admiral…” Relive a dramatic scene from Star Wars: The Empire Strikes Back as you build the first-ever LEGO® brick model of 
+            the Darth Vader Meditation Chamber (75296). Kick back and enjoy some quality time immersed in the rewarding building process and recreate lots of authentic details. 
+            The chamber opens and closes just like the real thing and features a big screen, control panels, a rotating seat for Darth Vader and a place for General Veers to stand opposite him.`,
+            `Built for display<br>
+            This set includes LEGO minifigures of Darth Vader and General Veers. A nameplate completes an eye-catching centerpiece that is a nice size to display on a shelf 
+            or as a desk ornament and will enhance the decor of your home or workplace.`
+        ],
+        pieces: 663,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 69.99,
+        stock: 24,
+    },
+    {
+        id: 75304,
+        name: 'Darth Vader™ Helmet',
+        description: [
+            `Pay homage to the Dark Lord of the Sith with this collectible LEGO® Star Wars™ Darth Vader Helmet (75304). Immerse yourself in the complex building process and relive 
+            classic Star Wars saga scenes as you recreate the iconic shape and sinister details of the helmet in LEGO style.`,
+            `Designed for display<br>
+            The Darth Vader Helmet is a compact size and has a display stand with a nameplate to complete a striking centerpiece that will enhance the decor of your home or workplace. 
+            And check out the other new-for-May-2021 LEGO Star Wars build-to-display helmet: Scout Trooper Helmet (75305).`
+        ],
+        pieces: 834,
+        released: true,
+        theme1: 'star wars',
+        tags: ['helmets', 'movies'],
+        price: 79.99,
+        stock: 13,
+    },
+    {
+        id: 75306,
+        name: 'Imperial Probe Droid™',
+        description: [
+            `Recall epic Star Wars: The Empire Strikes Back action on the ice planet Hoth as you build the Imperial Probe Droid (75306). The droid has posable legs and comes with a 
+            transparent, brick-built pole to 'suspend' it above a buildable snowy scene.`,
+            `Striking centerpiece<br>
+            An information plaque completes a highly collectible display piece that looks impressive on an office desk or a mantelpiece at home.`
+        ],
+        pieces: 683,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 59.99,
+        stock: 19,
+    },
+    {
+        id: 75308,
+        name: 'R2-D2™',
+        description: [
+            `Relive classic Star Wars™ moments as you build this exceptionally detailed R2-D2 LEGO® droid figure. The brilliant new-for-May-2021 design is packed with authentic details, 
+            including a retractable mid-leg, rotating head, opening and extendable front hatches, a periscope that can be pulled up and turned, and Luke Skywalker's lightsaber hidden 
+            in a compartment in the head.`,
+            `Build and display<br>
+            This cool construction model comes with a buildable display stand, featuring an information plaque, R2-D2 LEGO droid figure and an exclusive Lucasfilm 50th anniversary 
+            LEGO brick, to complete an awesome centerpiece for your home or workplace.`
+        ],
+        pieces: 2314,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 239.99,
+        stock: 11,
+    },
+    {
+        id: 75309,
+        name: 'Republic Gunship™',
+        description: [
+            `Spark memories of the epic Battle of Geonosis in Star Wars: The Clone Wars with this Ultimate Collector Series build-and-display model of a Republic Gunship (75309). 
+            Authentic features of a Low Altitude Assault Transport (LAAT) vehicle are beautifully reproduced in LEGO® bricks, including the pilot cockpits, swing-out spherical gun turrets, 
+            2 cannons on top, super-long wings, opening sides and rear hatch and interior details.`,
+            `First choice for LEGO fans<br>
+            Voted for by LEGO fans to become an Ultimate Collector Series set, this awesome buildable model has a display stand with an information plaque, plus Clone Trooper Commander and 
+            Mace Windu LEGO minifigures. Step-by-step instructions are included so you can immerse yourself in the building challenge and enjoy the fun, creative process.`
+        ],
+        pieces: 3292,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies', 'space'],
+        price: 399.99,
+        stock: 5,
+    },
+    {
+        id: 75313,
+        name: 'AT-AT™',
+        description: [
+            `This is the AT-AT (75313) that every LEGO® Star Wars™ fan has been waiting for. This epic Ultimate Collector Series build-and-display model features posable legs and head, 
+            cannons with a realistic recoil action, rotating cannons, bomb-drop hatch and a hook to attach to Luke Skywalker's line, just like in the Battle of Hoth.`,
+            `Room for an Imperial army<br>
+            The AT-AT measures over 24.5 in. (62 cm) high and is easily opened to view the detailed interior. The main body has space for 40 LEGO minifigures (General Veers and Luke 
+            Skywalker are among 9 in this set), 4 speeder bikes (2 included) and the included E-Web heavy repeating blaster. The head/cockpit seats 2 LEGO minifigures and has space 
+            for 1 more. An information plaque completes a magnificent display piece.`
+        ],
+        pieces: 6785,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 849.99,
+        stock: 2,
+    },
+    {
+        id: 75318,
+        name: 'The Child',
+        description: [
+            `Turn the cuteness factor up to the max with this LEGO® Star Wars™ The Child (75318) build-and-display model. Authentic details of this popular character, affectionately 
+            known as Baby Yoda, are recreated in LEGO style, with posable head, ears and mouth for different expressions, plus the Child's favorite toy - a gearshift knob (element 
+            included) - for it to hold just like it does in Star Wars: The Mandalorian.`
+        ],
+        pieces: 1073,
+        released: true,
+        theme1: 'star wars',
+        tags: ['tv-shows'],
+        price: 89.99,
+        stock: 9,
+    },
+    {
+        id: 75327,
+        name: 'Luke Skywalker™ (Red Five) Helmet',
+        description: [
+            `Feel the power of your creative Force as you build the collectible Luke Skywalker (Red Five) Helmet (75327). This buildable model of Luke's iconic pilot headgear is the 
+            first-ever hero helmet or open helmet in the LEGO® Star Wars™ collection. Channel your inner Jedi to recreate authentic details, including a brick-built microphone and 
+            interior padding, plus translucent red visor elements. Add the integrated stand with nameplate to complete a striking display piece.`
+        ],
+        pieces: 675,
+        released: true,
+        theme1: 'star wars',
+        tags: ['helmets', 'movies'],
+        price: 69.99,
+        stock: 13,
+    },
+    {
+        id: 75328,
+        name: 'The Mandalorian™ Helmet',
+        description: [
+            `Attention Star Wars: The Mandalorian fans: get into your creative zone and focus on building this collectible LEGO® Star Wars™ replica of The Mandalorian's helmet (75328). 
+            The metallic sheen of beskar armor is cleverly recreated with drum-lacquered elements, while bricks in different shades of gray allow you to highlight the authentic replica's 
+            contours. Add the brick-built display stand with nameplate to complete a striking centerpiece for your home or workplace.`
+        ],
+        pieces: 584,
+        released: true,
+        theme1: 'star wars',
+        tags: ['helmets', 'tv-shows'],
+        price: 69.99,
+        stock: 7,
+    },
+    {
+        id: 75329,
+        name: 'Death Star™ Trench Run Diorama',
+        description: [
+            `Channel Jedi-like focus and recreate a memorable moment from Star Wars: A New Hope with this Death Star Trench Run Diorama (75329) building set. Capture a snapshot of that 
+            gripping scene in intricate detail, from the surface of the Death Star to the brick-built models of Darth Vader's TIE Advanced fighter and 2 TIE fighters in hot pursuit of 
+            Luke Skywalker's X-wing. Attach the plaque bearing Darth Vader's words (“The Force is strong with this one”) to complete a nostalgia-inspiring centerpiece.`
+        ],
+        pieces: 665,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 69.99,
+        stock: 17,
+    },
+    {
+        id: 75330,
+        name: 'Dagobah™ Jedi™ Training Diorama',
+        description: [
+            `Focus, you must, to construct this LEGO® Star Wars™ Dagobah Jedi Training Diorama (75330). This brick-built display model is packed with instantly recognizable details 
+            from an iconic Star Wars: The Empire Strikes Back scene. Recreate Yoda's hut in the swamps of Dagobah, and the wing of Luke Skywalker's sunken X-wing. Add the LEGO minifigures 
+            of Yoda and Luke Skywalker, plus an R2-D2 LEGO droid figure (Luke and R2-D2 feature new-for-May-2022 decoration). To complete a nostalgic display piece, attach the plaque 
+            bearing Yoda's words: “Do. Or do not. There is no try.”`
+        ],
+        pieces: 1000,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 89.99,
+        stock: 14,
+    },
+    {
+        id: 75335,
+        name: 'BD-1™',
+        description: [
+            `Star Wars Jedi: Fallen Order players can relive their epic adventures when they build and display this LEGO® Star Wars™ version of exploration droid BD-1 (75335). The 
+            head of this posable brick-built model can be tilted back, forward and sideways for maximum cuteness. Fans will be delighted by other authentic details, such as the 
+            translucent elements on the back of the head that indicate BD-1's mood, plus a compartment for stim canister elements. An information sign and a BD-1 LEGO figure that 
+            can be attached next to the sign complete a charming centerpiece.`
+        ],
+        pieces: 1062,
+        released: true,
+        theme1: 'star wars',
+        tags: ['games'],
+        price: 99.99,
+        stock: 8,
+    },
+    {
+        id: 75339,
+        name: 'Death Star™ Trash Compactor Diorama',
+        description: [
+            `Begin a creative mission to recreate a legendary Star Wars: A New Hope scene with this Death Star Trash Compactor Diorama (75339) set. It is packed with authentic 
+            details, including moving walls and a dianoga's head. Place Luke Skywalker and Han Solo (both dressed as Stormtroopers), Princess Leia and Chewbacca inside Garbage 
+            Compactor 3263827, and C-3PO and R2-D2 at the compactor's controls trying to switch it off. Attach the plaque with Han Solo's dry quip ("One thing's for sure. 
+            We're all gonna be a lot thinner.") to complete a nostalgic display piece.`
+        ],
+        pieces: 802,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 89.99,
+        stock: 11,
+    },
+    {
+        id: 75341,
+        name: "Luke Skywalker's Landspeeder™",
+        description: [
+            `Be transported to the desert planet of Tatooine as you build the first-ever LEGO® Star Wars™ Ultimate Collector Series version of Luke Skywalker's Landspeeder (75341). 
+            Use new building techniques and custom-made LEGO elements to recreate this iconic vehicle in intricate detail. From the cockpit windshield to the turbine engine missing 
+            its cover, it has everything you remember from Star Wars: A New Hope.`
+        ],
+        pieces: 1890,
+        released: true,
+        theme1: 'star wars',
+        tags: ['movies'],
+        price: 239.99,
+        stock: 7,
+    },
+    {
+        id: 75343,
+        name: 'Dark Trooper™ Helmet',
+        description: [
+            `Take a trip to the dark side with this buildable LEGO® Star Wars™ Dark Trooper Helmet (75343) model for adults. Enjoy mindful quality time recreating the distinctive 
+            contours of the helmet as seen in Star Wars: The Mandalorian Season 2. Evil red eyes, constructed using translucent red LEGO elements, and the slight forward tilt of 
+            the helmet on its brick-built stand deliver an authentic menacing look.`
+        ],
+        pieces: 693,
+        released: true,
+        theme1: 'star wars',
+        tags: ['helmets', 'tv-shows'],
+        price: 69.99,
+        stock: 16,
+    }
+]
