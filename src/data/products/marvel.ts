5891a98db270d4b914fb8f8223574fbc466d40eb
M. Valkenburg <m.valkenburg@live.nl>
2022-08-23 14:32:22 +0200

    added products

---
+++ src/data/products/marvel.ts
@@ -0,0 +1,162 @@
+import { Product } from './_productType';
+
+export let marvel: Product[] = [
+    {
+        id: 76178,
+        name: 'Daily Bugle',
+        description: [
+            `LEGO® Marvel Spider-Man Daily Bugle (76178) brings together a cast of 25 classic characters from the Spiderverse in a stunning build-and-display construction project for adults.`,
+            `A 4-story celebration of the Marvel Universe
+            Measuring over 32 in. (82 cm) high, this 3,772-piece recreation of the Daily Bugle office block provides a towering backdrop for an all-star cast of 
+            Marvel heroes and villains, many new to this set! The building is filled with authentic details, fascinating features and classic comic-book action - 
+            from the New York taxi cab outside to the penthouse office of the Bugle's Editor in Chief. Marvel enthusiasts will find the model challenging but highly 
+            rewarding to build. Once it's completed, the removable floors, roof and detachable facades reveal this Marvel tribute for all to admire.`
+        ],
+        pieces: 3772,
+        released: true,
+        theme1: 'marvel',
+        tags: ['building', 'super heroes'],
+        price: 349.99,
+        stock: 5,   
+    },
+    {
+        id: 76187,
+        name: 'Venom',
+        description: [
+            `Stretch your construction skills and capture forever the distinctive, razor-toothed features of an iconic Marvel super villain with LEGO® Marvel Spider-Man Venom (76187).`,
+            `A detailed replica to build and display
+            Fans of Venom and the Spider-Man movies will relish a rewarding building experience as they recreate the authentic features of the gruesome alien symbiote. 
+            The oversized jaw packed with dagger-like teeth, the long, twisting tongue and the low, black brow combine to create an unmissable display piece. With a 
+            stylish name plaque attached to the sturdy base, this remarkable model will look even more impressive displayed alongside other LEGO comic-book creations.`
+        ],
+        pieces: 565,
+        released: true,
+        theme1: 'marvel',
+        tags: ['helmets', 'super heroes'],
+        price: 69.99,
+        stock: 12,
+    },
+    {
+        id: 76191,
+        name: 'Infinity Gauntlet',
+        description: [
+            `LEGO® Marvel Infinity Gauntlet (76191) is a challenging construction project that rewards the builder with an impressive display piece that perfectly evokes Marvel's captivating style.`,
+            `Collectible Marvel movie model<br>
+            This LEGO brick recreation of the iconic Infinity Gauntlet featured in the Marvel Studios' Avengers: Infinity War and Avengers: Endgame movies is certain 
+            to attract attention and interest wherever it is displayed. With movable fingers, vibrantly colored Infinity Stones and a descriptive tablet attached to a 
+            sturdy base, this golden gauntlet provides an engrossing construction challenge and a finished model that will captivate all who see it.`
+        ],
+        pieces: 590,
+        released: true,
+        theme1: 'marvel',
+        tags: ['super heroes'],
+        price: 79.99,
+        stock: 7,
+    },
+    {
+        id: 76193,
+        name: "The Guardians' Ship",
+        description: [
+            `LEGO® Marvel The Guardians' Ship (76193) is perfect for anyone aged 14 and up who is serious about the Marvel Avengers movies, or just loves making cool models.`,
+            `Build-and-display Marvel spaceship
+            Fans can recreate the authentic detail and awesome style of one of the most iconic spaceships from the Marvel Avengers movies. The opening cockpit has 
+            room for all the iconic LEGO Marvel minifigures included in the set: Star-Lord, Groot, Rocket, Mantis, Thor and a Chitauri warrior. Fun features include 
+            a weapon store, computer and a place for the heroes to snack on coffee and donuts. A rotatable stand allows the spaceship to be positioned for dynamic 
+            display and endless imaginative adventures. And, when the action stops, it looks supercool on display in any kid's room.`
+        ],
+        pieces: 1901,
+        released: true,
+        theme1: 'marvel',
+        tags: ['space', 'super heroes'],
+        price: 159.99,
+        stock: 10,
+    },
+    {
+        id: 76199,
+        name: 'Carnage',
+        description: [
+            `LEGO® Marvel Spider-Man Carnage (76199) takes your construction skills into the Marvel Universe as you recreate the nightmarish features of the alien super villain.`,
+            `Authentic details to build and display
+            From the low, red brow to the protruding jaw packed with bayonet-like teeth, this LEGO brick replica of Carnage provides Marvel movie fans and model-making enthusiasts 
+            with a building experience that is challenging yet highly rewarding. With a stylish name plaque attached to the sturdy base, this remarkable model will look even more 
+            impressive displayed alongside other LEGO comic-book creations.`
+        ],
+        pieces: 546,
+        released: true,
+        theme1: 'marvel',
+        tags: ['helmets', 'super heroes'],
+        price: 59.99,
+        stock: 14,
+    },
+    {
+        id: 76209,
+        name: "Thor's Hammer",
+        description: [
+            `Celebrate Marvel Studios' Infinity Saga with LEGO® Marvel Thor's Hammer (76209). Designed with adult model makers in mind, this premium display piece perfectly 
+            evokes the style and spirit of the Marvel Cinematic Universe.`,
+            `A lasting reminder of an epic saga
+            Marvel enthusiasts will recognize this authentic recreation as Mjölnir, the magical weapon used to devastating effect by Thor throughout Marvel Studios' 23-movie 
+            Infinity Saga. The 18 in. (46 cm) tall model of the iconic hammer rests securely on a stand with an information plate displaying the word 'Mjölnir' and the 
+            Infinity Saga logo. A Thor minifigure is included, plus a captivating feature found in the head of the hammer. An opening element reveals the storage space for 
+            small-scale models of the Infinity Gauntlet, Tesseract and Odin's Fire. With 979 pieces, this challenging and rewarding model is sure to provide great pleasure 
+            and satisfaction long after the construction work has been completed.`
+        ],
+        pieces: 979,
+        released: true,
+        theme1: 'marvel',
+        tags: ['super heroes'],
+        price: 99.99,
+        stock: 8,
+    },
+    {
+        id: 76217,
+        name: 'I am Groot',
+        description: [
+            `Put a movable model of Baby Groot from the Marvel Cinematic Universe into the hands of kids aged 10 and up with LEGO® Marvel I am Groot (76217).`,
+            `Play-and-display Groot
+            Every fan of Marvel Studios' Guardians of the Galaxy Vol. 2 will know the famous scene where Baby Groot dances. Now kids can recreate this scene and others with 
+            this detailed, movable recreation of the lovable and mischievous toddler. The buildable model is easy to position and pose, and there's even a pretend cassette 
+            tape included for make-believe music! In addition, the free LEGO Building Instructions app lets kids view, zoom and rotate the model as they build, providing an 
+            amazing sense of immersion and interaction during the construction process.`
+        ],
+        pieces: 476,
+        released: true,
+        theme1: 'marvel',
+        tags: ['super heroes'],
+        price: 54.99,
+        stock: 21,
+    },
+    {
+        id: 76218,
+        name: 'Sanctum Sanctorum',
+        description: [
+            `Capture the magic of Doctor Strange with the stunning LEGO® Marvel Sanctum Sanctorum (76218), a celebration of the Master of the Mystic Arts - 
+            designed with adult enthusiasts in mind.`,
+            `A 3-story tribute to Doctor Strange<br>
+            This 2,708-piece recreation of the Sanctum Sanctorum, the notorious residence of Doctor Strange, incorporates classic scenes from Marvel Studios' Avengers: 
+            Infinity War and Doctor Strange in the Multiverse of Madness. The top story houses the museum of mystical collections, the middle section contains the library 
+            and at ground-level there are familiar features both inside and out. The set includes 9 iconic minifigures - Doctor Strange, Wong, Iron Man, Spider-Man, 
+            Ebony Maw, Master Mordo, Sinister Strange, Dead Strange and The Scarlet Witch - a host of authentic accessories and several customizable elements, which ensure 
+            this rewarding project delivers great pleasure long after the construction work has finished.`
+        ],
+        pieces: 2708,
+        released: true,
+        theme1: 'marvel',
+        tags: ['building', 'super heroes'],
+        price: 249.99,
+        stock: 7,
+    },
+    {
+        id: 76223,
+        name: 'Nano Gauntlet',
+        description: [
+            ``
+        ],
+        pieces: 675,
+        released: true,
+        theme1: 'marvel',
+        tags: ['super heroes'],
+        price: 69.99,
+        stock: 18,
+    }
+]
