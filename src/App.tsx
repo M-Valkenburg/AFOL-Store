be3b2fed303ad752c2ac1051f2473b47bfd79db9
M. Valkenburg <m.valkenburg@live.nl>
2022-08-29 14:24:38 +0200

    removed unused router link dependancy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
