b0419d6ea4c38204dfb953c83b3fc22769c4b770
M. Valkenburg <m.valkenburg@live.nl>
2022-08-29 12:31:46 +0200

    added/changes imports, added route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.scss';
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
