d73b8020bde6d82a786f1503c293e686e23b3810
M. Valkenburg <m.valkenburg@live.nl>
2022-08-30 11:34:33 +0200

    Added navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import { Header } from "./components/Header/Header";
+import { Navigation } from "./components/Navigation/Navigation"
 import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
