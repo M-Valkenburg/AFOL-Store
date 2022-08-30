0147f2c15daa4fa03b6f1fc2908d76270fc68444
M. Valkenburg <m.valkenburg@live.nl>
2022-08-30 11:35:21 +0200

    added banner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./header.scss";
+import banner from "../../assets/banner/banner.webp"
 
 export const Header = () => {
     return (
         <header>
-            
+            <img src={banner} alt="banner" />
         </header>
     )
 };
