de9b9037dc672ca000491857019d7e8d3f5b8d79
M. Valkenburg <m.valkenburg@live.nl>
2022-08-31 16:21:11 +0200

    imported and added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import { Header } from "./components/Header/Header";
 import { Navigation } from "./components/Navigation/Navigation"
+import { Footer } from "./components/Footer/Footer";
 import { Home } from "./components/Home/Home";
 import { Themes } from "./components/Themes/Themes";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="themes" element={<Themes />} />
         <Route path="*" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
