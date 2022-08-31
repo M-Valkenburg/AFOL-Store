b55d8c3063e7e291b04dc0e9d9aa7437f02a3b7c
M. Valkenburg <m.valkenburg@live.nl>
2022-08-31 16:06:43 +0200

    Link to homepage for banner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./header.scss";
 import banner from "../../assets/banner/banner.webp"
 
 export const Header = () => {
     return (
         <header>
-            <img src={banner} alt="banner" />
+            <Link to="/"><img src={banner} alt="banner" aria-label="home"/></Link>
         </header>
     )
 };
