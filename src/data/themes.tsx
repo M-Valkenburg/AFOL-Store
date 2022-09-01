7ae5e1dc84f920e4666fee3ac627a6685bcce486
M. Valkenburg <m.valkenburg@live.nl>
2022-09-01 14:43:56 +0200

    themes + images

---
+++ src/data/themes.tsx
@@ -0,0 +1,73 @@
+import architecture from "../assets/themes/Architecture.png";
+import architectureLogo from "../assets/logos/Architecture.png";
+import art from "../assets/themes/Art.png";
+import artLogo from "../assets/logos/Art.png";
+import DC from "../assets/themes/DC.jpg";
+import DCLogo from "../assets/logos/DC.png";
+import disney from "../assets/themes/Disney.jpg";
+import disneyLogo from "../assets/logos/Disney.png";
+import harryPotter from "../assets/themes/Harry Potter.jpg";
+import harryPotterLogo from "../assets/logos/Harry Potter.png";
+import icons from "../assets/themes/Icons.jpg";
+import iconsLogo from "../assets/logos/Icons.png";
+import ideas from "../assets/themes/Ideas.png";
+import ideasLogo from "../assets/logos/Ideas.png";
+import marvel from "../assets/themes/Marvel.png";
+import marvelLogo from "../assets/logos/Marvel.png";
+import starWars from "../assets/themes/Star Wars.jpg";
+import starWarsLogo from "../assets/logos/Star Wars.png";
+import technic from "../assets/themes/Technic.jpg";
+import technicLogo from "../assets/logos/Technic.png";
+
+export const themes: {name: string, img: string, logo: string}[] = [
+    {
+        name: "Architecture",
+        img: architecture,
+        logo: architectureLogo
+    },
+    {
+        name: "Art",
+        img: art,
+        logo: artLogo
+    },
+    {
+        name: "DC",
+        img: DC,
+        logo: DCLogo
+    },
+    {
+        name: "Disney",
+        img: disney,
+        logo: disneyLogo
+    },
+    {
+        name: "Harry Potter",
+        img: harryPotter,
+        logo: harryPotterLogo
+    },
+    {
+        name: "Icons",
+        img: icons,
+        logo: iconsLogo
+    },
+    {
+        name: "Ideas",
+        img: ideas,
+        logo: ideasLogo
+    },
+    {
+        name: "Marvel",
+        img: marvel,
+        logo: marvelLogo
+    },
+    {
+        name: "Star Wars",
+        img: starWars,
+        logo: starWarsLogo
+    },
+    {
+        name: "Technic",
+        img: technic,
+        logo: technicLogo
+    }
+]
