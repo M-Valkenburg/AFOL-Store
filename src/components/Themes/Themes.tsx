e2c2db8019c7a393bd81d96b3d9c1240513d6d0e
M. Valkenburg <m.valkenburg@live.nl>
2022-09-01 18:34:01 +0200

    added themes to page

--- src/components/Themes/Themes.tsx
+++ src/components/Themes/Themes.tsx
@@ -1,9 +1,22 @@
 import "./themes.scss";
+import { themes } from "../../data/themes";
 
 export const Themes = () => {
     return (
         <div className="themes">
-
+            <h2>Themes</h2>
+            <div className="themes-container">
+                {themes.map(theme => {
+                    return (
+                        <div className="theme" key={theme.name}>
+                            <img className="theme-img" src={theme.img} alt={theme.name}/>
+                            <div className="logo-container">
+                                <img className="logo" src={theme.logo} alt={theme.name}/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 };
