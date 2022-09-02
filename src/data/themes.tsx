50877d3982ef5fb95bf0d3b0c4a863e08c64de85
M. Valkenburg <m.valkenburg@live.nl>
2022-09-02 12:17:08 +0200

    changed image extensions to .webp

--- src/data/themes.tsx
+++ src/data/themes.tsx
@@ -1,22 +1,22 @@
-import architecture from "../assets/themes/Architecture.png";
+import architecture from "../assets/themes/Architecture.webp";
 import architectureLogo from "../assets/logos/Architecture.png";
-import art from "../assets/themes/Art.png";
+import art from "../assets/themes/Art.webp";
 import artLogo from "../assets/logos/Art.png";
-import DC from "../assets/themes/DC.jpg";
+import DC from "../assets/themes/DC.webp";
 import DCLogo from "../assets/logos/DC.png";
-import disney from "../assets/themes/Disney.jpg";
+import disney from "../assets/themes/Disney.webp";
 import disneyLogo from "../assets/logos/Disney.png";
-import harryPotter from "../assets/themes/Harry Potter.jpg";
+import harryPotter from "../assets/themes/Harry Potter.webp";
 import harryPotterLogo from "../assets/logos/Harry Potter.png";
-import icons from "../assets/themes/Icons.jpg";
+import icons from "../assets/themes/Icons.webp";
 import iconsLogo from "../assets/logos/Icons.png";
-import ideas from "../assets/themes/Ideas.png";
+import ideas from "../assets/themes/Ideas.webp";
 import ideasLogo from "../assets/logos/Ideas.png";
-import marvel from "../assets/themes/Marvel.png";
+import marvel from "../assets/themes/Marvel.webp";
 import marvelLogo from "../assets/logos/Marvel.png";
-import starWars from "../assets/themes/Star Wars.jpg";
+import starWars from "../assets/themes/Star Wars.webp";
 import starWarsLogo from "../assets/logos/Star Wars.png";
-import technic from "../assets/themes/Technic.jpg";
+import technic from "../assets/themes/Technic.webp";
 import technicLogo from "../assets/logos/Technic.png";
 
 export const themes: {name: string, img: string, logo: string}[] = [
