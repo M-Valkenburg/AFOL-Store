146f27ba5096057e6577022baba78849b69f9bdd
M. Valkenburg <m.valkenburg@live.nl>
2022-09-02 13:40:02 +0200

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import { Header } from "./components/Header/Header";
 import { Navigation } from "./components/Navigation/Navigation"
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./components/Home/Home";
-import { Themes } from "./components/Themes/Themes";
+
+const Themes = React.lazy(() => import("./components/Themes/Themes")
+  .then(({ Themes }) => ({ default: Themes }))
+);
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="themes" element={<Themes />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="themes" element={<Themes />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
