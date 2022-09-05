0b36124c31cc20c92db7a786c73bb5dc3bf3bba5
M. Valkenburg <m.valkenburg@live.nl>
2022-09-05 19:37:59 +0200

    Mobile navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,26 +1,41 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./navigation.scss";
+import menu from "../../assets/icons/menu.svg"
 import search from "../../assets/icons/search.svg";
 import cart from "../../assets/icons/cart.svg";
 import account from "../../assets/icons/account.svg";
 
 export const Navigation = () => {
+    const handleClick = () => {
+        let dropdown = document.getElementById('menu-links') as HTMLElement;
+        
+        dropdown.style.display === 'none'
+            ? dropdown.style.display = 'block'
+            : dropdown.style.display = 'none';
+    };
+
     return (
-        <nav>
+        <nav className="nav-menu">
             <div>
-                <Link to="/themes" className="nav-hover">Themes</Link>
-                <Link to="/" className="nav-hover">Interest</Link>
-                <Link to="/" className="nav-hover">Sale</Link>
+                <div className="menu-button" onClick={handleClick}>
+                    <img src={menu} alt="menu"/>
+                </div>
+                <div id="menu-links">
+                    <Link to="/themes" className="nav-hover">Themes</Link>
+                    <Link to="/" className="nav-hover">Interest</Link>
+                    <Link to="/" className="nav-hover">Sale</Link>
+                </div>
             </div>
-            <div>
+            <div className="nav-icons">
                 <Link to="" className="nav-hover" aria-label="search">
-                    <img src={search} alt="search"></img> Search
+                    <img src={search} alt="search"></img><span className="nav-name"> Search</span>
                 </Link>
                 <Link to="" className="nav-hover" aria-label="cart">
-                    <img src={cart} alt="cart"></img> Cart
+                    <img src={cart} alt="cart"></img><span className="nav-name"> Cart</span>
                 </Link>
                 <Link to="" className="nav-hover" aria-label="account">
-                    <img src={account} alt="account"></img> Login
+                    <img src={account} alt="account"></img><span className="nav-name"> Login</span>
                 </Link>
             </div>
         </nav>
