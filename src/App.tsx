6b3501cef1e61dcd4a1825e6179e79689322afcb
M. Valkenburg <m.valkenburg@live.nl>
2022-09-06 17:03:37 +0200

    updated imports and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
-import { Header } from "./components/Header/Header";
-import { Navigation } from "./components/Navigation/Navigation"
-import { Footer } from "./components/Footer/Footer";
-import { Home } from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation"
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
-const Themes = React.lazy(() => import("./components/Themes/Themes")
-  .then(({ Themes }) => ({ default: Themes }))
-);
+const Themes = React.lazy(() => import("./components/Themes/Themes"));
+const Theme = React.lazy(() => import("./components/Themes/Theme"));
 
 function App() {
   return (
@@ -19,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="themes" element={<Themes />} />
+          <Route path="themes/:themeId" element={<Theme />}></Route>
           <Route path="*" element={<Home />} />
         </Routes>
       </Suspense>
