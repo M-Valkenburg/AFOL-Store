03cddd7115985121d3030794549cc65afb73313e
M. Valkenburg <m.valkenburg@live.nl>
2022-09-07 11:36:46 +0200

    initiated slice

---
+++ src/store/themeSlice.tsx
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState: {
+        theme: ''
+    },
+    reducers: {}
+});
+
+export default themeSlice;
