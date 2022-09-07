1b0e0c7f3ef815076e20c6c7de06c14ebf6567c8
M. Valkenburg <m.valkenburg@live.nl>
2022-09-07 16:21:56 +0200

    variable for path name + path whitespace removal

--- src/components/Themes/Themes.tsx
+++ src/components/Themes/Themes.tsx
@@ -8,8 +8,9 @@ const Themes = () => {
             <h2>Themes</h2>
             <div className="themes-container">
                 {themes.map(theme => {
+                    const pathName: string = theme.name.replace(/\s+/g, '').toLowerCase();
                     return (
-                        <Link to={`/themes/${theme.name.toLowerCase()}`} className="theme" key={theme.name}>
+                        <Link to={`/themes/${pathName}`} className="theme" key={theme.name}>
                             <img className="theme-img" src={theme.img} alt={theme.name} loading="lazy"/>
                             <div className="logo-container">
                                 <img className="logo" src={theme.logo} alt={theme.name} loading="lazy"/>
