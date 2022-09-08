766993da7204b52bb3b8e5e2ab577787b203a51b
M. Valkenburg <m.valkenburg@live.nl>
2022-09-08 18:42:03 +0200

    rendered products

--- src/components/Themes/Theme.tsx
+++ src/components/Themes/Theme.tsx
@@ -1,8 +1,37 @@
+import { useLocation } from "react-router-dom";
 import './theme.scss';
+import { themes } from "../../data/themes";
 
 const Theme = () => {
+    const location = useLocation();
+    const pathName: string = location.pathname;
+    const strippedPath = pathName.replace('/themes/', '');
+    const theme = strippedPath.charAt(0) + strippedPath.slice(1);
+
+    const index = themes.findIndex(obj => {
+        return obj.path === theme;
+    })
+
     return (
-        <div className="theme-page"></div>
+        <div className="theme-page">
+            <h2>{themes[index].name}</h2>
+            <div className="products-container">
+                {themes[index].products.map(product => {
+                    return (
+                        <div className="product-tile" key={product.id}>
+                            <div className="prod-img">
+                                <img src={product.image} alt={product.name}/>
+                            </div>
+                            <div className="prod-name-container">
+                                <h4>{product.name}</h4>
+                            </div>
+                            <p>€ {product.price}</p>
+                            <button>Add to cart</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 };
 
