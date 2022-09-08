9268b16e114fc15ece7cd165e81b59b8003dcec5
M. Valkenburg <m.valkenburg@live.nl>
2022-09-08 15:19:28 +0200

    added ScrollToTop

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
+import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
