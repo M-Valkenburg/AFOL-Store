214636666495e136fff6bac48a0ae120864dcc6d
M. Valkenburg <m.valkenburg@live.nl>
2022-09-08 15:19:14 +0200

    scroll to top after clicking route link

---
+++ src/utils/ScrollToTop.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from 'react-router'
+
+const ScrollToTop = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+
+    return (null);
+}
+    
+export default ScrollToTop;
