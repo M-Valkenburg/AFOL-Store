b8f4d5831779fd59942aaddc5d9d7e93763612ac
M. Valkenburg <m.valkenburg@live.nl>
2022-09-08 11:49:45 +0200

    removed variable, changed to pathname of object

--- src/components/Themes/Themes.tsx
+++ src/components/Themes/Themes.tsx
@@ -8,9 +8,8 @@ const Themes = () => {
             <h2>Themes</h2>
             <div className="themes-container">
                 {themes.map(theme => {
-                    const pathName: string = theme.name.replace(/\s+/g, '').toLowerCase();
                     return (
-                        <Link to={`/themes/${pathName}`} className="theme" key={theme.name}>
+                        <Link to={`/themes/${theme.path}`} className="theme" key={theme.name}>
                             <img className="theme-img" src={theme.img} alt={theme.name} loading="lazy"/>
                             <div className="logo-container">
                                 <img className="logo" src={theme.logo} alt={theme.name} loading="lazy"/>
