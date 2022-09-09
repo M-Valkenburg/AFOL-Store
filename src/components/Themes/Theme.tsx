e8b3b11a4ff7fe988d8fbead937d9cee3c1dd0b8
M. Valkenburg <m.valkenburg@live.nl>
2022-09-09 19:44:20 +0200

    added conditional for discount percentage

--- src/components/Themes/Theme.tsx
+++ src/components/Themes/Theme.tsx
@@ -18,13 +18,14 @@ const Theme = () => {
                         <div className="product-tile" key={product.id}>
                             <Link to={`/product/${product.id}`} className="prod-img">
                                 <img src={product.image} alt={product.name}/>
+                                {product.sale ? <div className="discount-percentage">-{product.discount}%</div> : ""}
                             </Link>
                             <div className="prod-name-container">
                                 <Link to={`/product/${product.id}`}>{product.name}</Link>
                             </div>
                             <div className="prod-price">
-                                <p>€ {product.price}</p>
-                                <p className="sale-price"></p>
+                                <p className={product.sale ? "strikethrough" : ""}>€ {product.price}</p>
+                                {product.sale ? <p className="sale-price">{product.salePrice}</p> : ""}
                             </div>
                             <button>Add to cart</button>
                         </div>
