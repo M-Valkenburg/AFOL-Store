7fb46b1dcfdc818902b76c7f6ff9a35dddb1a449
M. Valkenburg <m.valkenburg@live.nl>
2022-09-12 13:49:34 +0200

    joined all product arrays to one array

---
+++ src/data/products.ts
@@ -0,0 +1,28 @@
+import { Product } from "./products/_productType";
+import architectureProd from "./products/architecture";
+import artProd from "./products/art";
+import dcProd from "./products/dc";
+import disneyProd from "./products/disney";
+import harrypotterProd from "./products/harrypotter";
+import iconsProd from "./products/icons";
+import ideasProd from "./products/ideas";
+import marvelProd from "./products/marvel";
+import starwarsProd from "./products/starwars";
+import technicProd from "./products/technic";
+
+let productsArray: Product[] = [];
+const products = productsArray.concat(
+    architectureProd,
+    artProd,
+    dcProd,
+    dcProd,
+    disneyProd,
+    harrypotterProd,
+    iconsProd,
+    ideasProd,
+    marvelProd,
+    starwarsProd,
+    technicProd
+);
+
+export default products;
