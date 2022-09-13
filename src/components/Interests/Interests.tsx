53e2077ccfde2de0173015f29b0ed515b3a85442
M. Valkenburg <m.valkenburg@live.nl>
2022-09-13 19:50:34 +0200

    renamed fron interest to interests

---
+++ src/components/Interests/Interests.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import "./interests.scss";
+import "../Themes/themes.scss";
+import { tags } from "../../data/tags";
+
+const Interest = () => {
+    return (
+        <div className="themes interest">
+            <h2>Interest</h2>
+            <div className="themes-container">
+                {tags.map(tag => {
+                    return (
+                        <Link to={`/interest/${tag.path}`} className="theme" key={tag.name}>
+                            <img className="theme-img" src={tag.image} alt={tag.name} loading="lazy"/>
+                            <div className="logo-container">
+                                <span>{tag.name}</span>
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
+        </div>
+    )
+};
+
+export default Interest;
