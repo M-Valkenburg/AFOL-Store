f09b5f2841416a928a7f5595976e628f2d930bcb
M. Valkenburg <m.valkenburg@live.nl>
2022-09-15 16:19:52 +0200

    added provider and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
+import { store } from './store/store';
 import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
