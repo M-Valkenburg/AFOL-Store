9cbe29985aef5465694cb96402a8407ec32708f3
M. Valkenburg <m.valkenburg@live.nl>
2022-09-15 16:28:34 +0200

    store file including cartReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import cartReducer from "./cartSlice";
 
 export const store = configureStore({
   reducer: {
-
+    cart: cartReducer
   }
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
