56d35e75d32d4d039508f36ec72b83398d6c2638
M. Valkenburg <m.valkenburg@live.nl>
2022-09-16 17:06:13 +0200

    added more states, updated new states in reducers

--- src/store/cartSlice.tsx
+++ src/store/cartSlice.tsx
@@ -4,12 +4,20 @@ import { Product } from "../data/products/_productType";
 
 export interface CartState {
     cart: {product: Product, qty: number}[]
-    items: number;
+    items: number,
+    value: number,
+    VAT: number
+    shipping: number,
+    total: number
 }
 
 const initialstate: CartState = {
     cart: [],
-    items: 0
+    items: 0,
+    value: 0,
+    VAT: 0,
+    shipping: 5.95,
+    total: 0,
 };
 
 const cartSlice = createSlice({
@@ -23,9 +31,15 @@ const cartSlice = createSlice({
             if (index !== -1) {
                 state.cart[index].qty += 1;
                 state.items += 1;
+                state.value += (product.sale ? product.salePrice : product.price);
+                state.VAT = Number((state.value * 0.21).toFixed(2));
+                state.total = state.value + state.VAT;
             } else {
                 state.cart.push({product: action.payload, qty: 1});
                 state.items += 1;
+                state.value += (product.sale ? product.salePrice : product.price);
+                state.VAT = Number((state.value * 0.21).toFixed(2));
+                state.total = state.value + state.VAT;
             }
         },
         removeProduct: (state, action: PayloadAction<Product>) => {
@@ -34,20 +48,31 @@ const cartSlice = createSlice({
             
             state.items -= state.cart[index].qty;
             state.cart.splice(index, 1)
+            state.value -= (product.sale ? product.salePrice : product.price);
+            state.VAT = Number((state.value * 0.21).toFixed(2));
+            state.total = state.value + state.VAT;
         },
         increment: (state, action: PayloadAction<Product>) => {
             const product: Product = action.payload;
             const index: number = state.cart.findIndex(item => item.product.id === product.id);
             
             state.cart[index].qty += 1;
-            state.items += 1;    
+            state.items += 1;
+            state.value += (product.sale ? product.salePrice : product.price);
+            state.VAT = Number((state.value * 0.21).toFixed(2));
+            state.total = state.value + state.VAT;
         },
         decrement: (state, action: PayloadAction<Product>) => {
             const product: Product = action.payload;
             const index: number = state.cart.findIndex(item => item.product.id === product.id);
             
+            if (state.cart[index].qty === 1) return;
+
             state.cart[index].qty -= 1;
-            state.items -= 1;                
+            state.items -= 1;
+            state.value -= (product.sale ? product.salePrice : product.price);
+            state.VAT = Number((state.value * 0.21).toFixed(2));
+            state.total = state.value + state.VAT; 
         },
         changeQty: (state, action) => {},
     }
@@ -56,6 +81,10 @@ const cartSlice = createSlice({
 export default cartSlice.reducer;
 export const selectCart = (state: RootState) => state.cart.cart;
 export const selectItems = (state: RootState) => state.cart.items;
+export const selectValue = (state: RootState) => state.cart.value;
+export const selectVAT = (state: RootState) => state.cart.VAT;
+export const selectShipping = (state: RootState) => state.cart.shipping;
+export const selectTotal = (state: RootState) => state.cart.total;
 
 export const { 
     addProduct, 
