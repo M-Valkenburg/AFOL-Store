4296cad2a01ac06494d7f7629fbd51905b8a6cd6
M. Valkenburg <m.valkenburg@live.nl>
2022-09-21 15:08:49 +0200

    added in-/decrease buttons + state

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import { useParams } from "react-router-dom";
-import ReactMarkdown from 'react-markdown';
 import "./product.scss";
+import ReactMarkdown from 'react-markdown';
 import { addProduct } from "../../store/cartSlice";
 import { useAppDispatch } from '../../store/hooks';
 import products from "../../data/products";
 import rehypeRaw from "rehype-raw";
+import plus from "../../assets/icons/plus.svg";
+import minus from "../../assets/icons/minus.svg";
 
 const Product = () => {
     const dispatch = useAppDispatch();
     const params = useParams();  
     const index = products.findIndex(obj => obj.id.toString() === params.productId);
     const product = products[index];
+
+    let [qty, setQty] = useState<number>(1);
+
+    const decrement = () => {
+        if (qty === 1 || qty < 1) return;
+        setQty(qty => qty -= 1);
+    };
+
+    const increment = () => {
+        if (qty === product.stock) return;
+        setQty(qty => qty += 1);
+    };
 
     return (
         <div className="product-page">
@@ -33,12 +48,26 @@ const Product = () => {
                                     ? <span className="unavailable">Out of stock</span>
                                     : <span className="available">Available now</span>
                         }
+                        <div className="product-qty">
+                            <button aria-label="decrease quantity" onClick={decrement}>
+                                <img src={minus} alt="decrease quantity"/>
+                            </button>
+                            <span className="value">{qty}</span>
+                            <button aria-label="increase quantity" onClick={increment}>
+                                <img src={plus} alt="increase quantity"/>
+                            </button>
+                        </div>
+                        <span className="product-stock">In stock: {product.stock}</span>
                         {
                             !product.released 
                                 ? ""
                                 : product.released && product.stock === 0 
                                     ? <button disabled>Add to cart</button>
-                                    : <button onClick={() => dispatch(addProduct(product))} aria-label="Add to cart">Add to cart</button>
+                                    : <button 
+                                        className="product-to-cart" 
+                                        aria-label="Add to cart"
+                                        onClick={() => dispatch(addProduct([product, qty]))} 
+                                    >Add to cart</button>
                         }
                         <p className="set-id">Set id: {product.id}</p>
                         <p className="pieces">Pieces: {product.pieces}</p>
