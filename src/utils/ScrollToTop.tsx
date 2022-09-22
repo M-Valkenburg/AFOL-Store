ddecaaf02e0276a0677d1ace40bbfa1119471fc5
M. Valkenburg <m.valkenburg@live.nl>
2022-09-22 17:26:46 +0200

    added return type

--- src/utils/ScrollToTop.tsx
+++ src/utils/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from 'react-router'
 
-const ScrollToTop = () => {
+const ScrollToTop = (): null => {
     const location = useLocation();
 
     useEffect(() => {
