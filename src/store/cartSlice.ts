d0f887b5524535910553ef8be7f1efb2bb90a529
M. Valkenburg <m.valkenburg@live.nl>
2022-09-23 16:57:28 +0200

    corrected RootState import

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../store/store';
+import { RootState } from './store';
 import { Product } from "../data/products/_productType";
 
 export interface CartState {
