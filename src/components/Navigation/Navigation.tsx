26c0e1e3bdddbfd9930aa6850b7976f7c6261dca
M. Valkenburg <m.valkenburg@live.nl>
2022-10-03 11:41:39 +0200

    working search function

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,40 @@
-import { Link } from "react-router-dom";
-import { useAppSelector } from '../../store/hooks';
-import { selectItems } from "../../store/cartSlice";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import "./navigation.scss";
+import { selectItems } from "../../store/cartSlice";
+import { selectSearchTerm, setSearchTerm } from "../../store/productSlice";
 import menu from "../../assets/icons/menu.svg"
 import search from "../../assets/icons/search.svg";
 import cart from "../../assets/icons/cart.svg";
-import account from "../../assets/icons/account.svg";
 
 const Navigation = () => {
-    const cartTotal = useAppSelector(selectItems);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const cartTotal = useAppSelector<number>(selectItems);
+    const searchTerm = useAppSelector<string>(selectSearchTerm);
+
+    const [ searchValue, setSearchValue ] = useState<string>('');
+
+    const getSearch = (e: any) => {
+        if (e === '') return;
+        setSearchValue(e.target.value.toLowerCase());
+    };
+
+    useEffect(() => {
+        setSearchValue(searchTerm);
+    }, [searchTerm]);
+
+    const setSearch = (e: any) => {
+        e.preventDefault();
+        dispatch(setSearchTerm(searchValue));
+
+        navigate({
+            pathname: 'search',
+            search: `?q=${searchValue}`
+        });
+    };
 
     return (
         <nav>
@@ -24,15 +50,22 @@ const Navigation = () => {
                 </div>
             </div>
             <div className="nav-right">
-                <Link to="" className="nav-hover" aria-label="search">
-                    <img src={search} alt="search"></img><span className="nav-name"> Search</span>
-                </Link>
+                <form className="search" onSubmit={setSearch}>
+                    <input
+                        name="search"
+                        type="text" 
+                        placeholder="Search..." 
+                        defaultValue={searchTerm} 
+                        onChange={getSearch}
+                        required
+                    />
+                    <button type="submit" aria-label="search products" onSubmit={setSearch}>
+                        <img src={search} alt="search"></img>
+                    </button> 
+                </form>
                 <Link to="/cart" className="nav-hover" aria-label="cart">
                     <img src={cart} alt="cart"></img><span className="nav-name"> Cart</span><span className="cartTotal"> ({cartTotal})</span>
                 </Link>
-                <Link to="" className="nav-hover" aria-label="account">
-                    <img src={account} alt="account"></img><span className="nav-name"> Login</span>
-                 </Link>
             </div>
         </nav>
     )
