3ec5476a43271242f00be5844b179bb488cf4611
M. Valkenburg <m.valkenburg@live.nl>
2022-10-04 15:00:35 +0200

    placeholder component foor footer links

---
+++ src/components/Placeholder/Placeholder.tsx
@@ -0,0 +1,75 @@
+import "./placeholder.scss";
+
+const Placeholder = () => {
+    return (
+        <div className="placeholder">
+            <h2>Lorum Ipsum</h2>
+            <div className="placeholder-text">
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Vitae suscipit tellus mauris a diam maecenas sed enim ut. Accumsan tortor posuere ac ut consequat semper viverra. In eu mi 
+                    bibendum neque egestas congue quisque egestas. Sed enim ut sem viverra aliquet eget sit amet. Elementum sagittis vitae et 
+                    leo duis. Purus ut faucibus pulvinar elementum integer. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Eu 
+                    tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Feugiat pretium nibh ipsum consequat nisl. Quis imperdiet 
+                    massa tincidunt nunc. Mi quis hendrerit dolor magna. Ullamcorper eget nulla facilisi etiam dignissim. Morbi tincidunt ornare 
+                    massa eget. Lectus sit amet est placerat in. Vulputate mi sit amet mauris commodo quis imperdiet massa. Netus et malesuada 
+                    fames ac. Sagittis purus sit amet volutpat consequat mauris nunc congue. Pellentesque diam volutpat commodo sed egestas 
+                    egestas fringilla phasellus faucibus.
+                </p>
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Turpis egestas integer eget aliquet nibh praesent. Viverra nam libero justo laoreet sit amet. Gravida in fermentum et 
+                    sollicitudin ac orci phasellus egestas tellus. Proin sagittis nisl rhoncus mattis rhoncus urna. Fames ac turpis egestas 
+                    maecenas pharetra. In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Ullamcorper sit amet risus 
+                    nullam eget. Eget felis eget nunc lobortis mattis. Scelerisque in dictum non consectetur a erat nam at lectus. Sed blandit 
+                    libero volutpat sed cras ornare arcu dui vivamus.
+                </p>
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Congue nisi vitae suscipit tellus. Facilisi etiam dignissim diam quis. Volutpat diam ut venenatis tellus in metus vulputate 
+                    eu scelerisque. Amet aliquam id diam maecenas ultricies mi eget mauris. Mus mauris vitae ultricies leo. Sem integer vitae 
+                    justo eget magna fermentum iaculis eu. Orci eu lobortis elementum nibh tellus molestie nunc. Etiam non quam lacus suspendisse 
+                    faucibus. Magna ac placerat vestibulum lectus mauris ultrices eros in. Mi in nulla posuere sollicitudin aliquam ultrices. 
+                    Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Tellus elementum sagittis vitae et. Senectus et netus et 
+                    malesuada fames. Id neque aliquam vestibulum morbi. Duis at tellus at urna condimentum mattis pellentesque id nibh. Amet 
+                    massa vitae tortor condimentum. Et pharetra pharetra massa massa.
+                </p>
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Ipsum dolor sit amet consectetur adipiscing elit duis tristique. Facilisis leo vel fringilla est ullamcorper. Enim nec dui 
+                    nunc mattis enim. Non tellus orci ac auctor augue mauris augue neque. Ultrices in iaculis nunc sed augue. Donec pretium 
+                    vulputate sapien nec. Amet mauris commodo quis imperdiet massa tincidunt. Feugiat scelerisque varius morbi enim. Risus pretium 
+                    quam vulputate dignissim suspendisse in est. Sit amet nisl purus in. Ut diam quam nulla porttitor massa id neque aliquam. 
+                    Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Pretium lectus quam id leo. Scelerisque varius morbi enim 
+                    nunc faucibus. Sit amet consectetur adipiscing elit duis tristique sollicitudin. Morbi tristique senectus et netus et malesuada 
+                    fames ac. Imperdiet massa tincidunt nunc pulvinar. Ac felis donec et odio.
+                </p>
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. 
+                    Orci eu lobortis elementum nibh tellus molestie nunc non blandit. Ornare quam viverra orci sagittis eu. Ac placerat vestibulum 
+                    lectus mauris ultrices eros. In arcu cursus euismod quis viverra nibh. Nibh venenatis cras sed felis eget velit aliquet sagittis. 
+                    Maecenas volutpat blandit aliquam etiam erat. Tincidunt nunc pulvinar sapien et. Sed augue lacus viverra vitae. Ut etiam sit amet 
+                    nisl purus in mollis. Nam aliquam sem et tortor consequat id porta. Facilisis sed odio morbi quis commodo odio aenean sed. Nam 
+                    aliquam sem et tortor consequat id porta nibh venenatis. Senectus et netus et malesuada fames ac turpis. Facilisis magna etiam 
+                    tempor orci eu lobortis. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus et.
+                </p>
+                <h3>Lorum Ipsum</h3>
+                <p>
+                    Dolor sit amet consectetur adipiscing elit pellentesque habitant. Non odio euismod lacinia at. Nunc non blandit massa enim nec 
+                    dui nunc. Velit scelerisque in dictum non consectetur a erat nam at. In nibh mauris cursus mattis molestie a iaculis. Aenean sed 
+                    adipiscing diam donec adipiscing tristique risus nec. Morbi tempus iaculis urna id volutpat lacus laoreet. Mattis ullamcorper velit 
+                    sed ullamcorper morbi tincidunt. Malesuada pellentesque elit eget gravida cum sociis. Et netus et malesuada fames ac turpis egestas 
+                    sed tempus. Bibendum arcu vitae elementum curabitur vitae nunc. Laoreet non curabitur gravida arcu ac tortor. In egestas erat 
+                    imperdiet sed euismod nisi porta lorem mollis. Consequat nisl vel pretium lectus quam id leo. Tincidunt eget nullam non nisi. At 
+                    auctor urna nunc id cursus metus aliquam eleifend. Nunc sed augue lacus viverra vitae. Turpis egestas integer eget aliquet nibh 
+                    praesent tristique magna. Scelerisque felis imperdiet proin fermentum leo vel orci porta. Amet consectetur adipiscing elit duis 
+                    tristique sollicitudin nibh sit amet.
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default Placeholder;
